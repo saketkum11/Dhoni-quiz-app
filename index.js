09a29120a3c202c51eb2450f1cb3b61331aa1967
Saket kumar <46194436+saketkum11@users.noreply.github.com>
2021-09-16 09:10:14 +0000

    update in index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function play(question , answer){
 
   var userAnswer = readlineSync.question(question);
 
-  if(userAnswer.toUpperCase == answer.toUpperCase){
+  if(userAnswer == answer){
     console.log(chalk.green("right"));
     score = score + 1;
    
